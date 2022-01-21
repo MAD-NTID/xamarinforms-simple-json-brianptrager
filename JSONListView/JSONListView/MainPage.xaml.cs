5d6f78e5466566deec4eac099a8600088a24de14
bptrager <brian.trager@rit.edu>
2022-01-21 11:06:02 -0500

    Add project files.

---
+++ JSONListView/JSONListView/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using JSONListView.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace JSONListView
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Person> people;
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+
+            var assembly = typeof(MainPage).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("JSONListView.testmodel.json");
+            string text = "";
+            using (var reader = new StreamReader(stream))
+            {
+                text = reader.ReadToEnd();
+                List<Person> myObjects = JsonConvert.DeserializeObject<List<Person>>(text);
+                people = new ObservableCollection<Person>(myObjects);
+                MyListView.ItemsSource = people;
+            }
+        }
+    }
+}
---
+++ JSONListView/JSONListView/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSONListView.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Person
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+    }
+}
